e85079a6bf1176a06ad25b612ca21726d002d36d
Tony LaForgia <alforgia428@gmail.com>
2023-01-09 18:44:44 -0600

    type def comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
+    let  x = 5;
+    let x = x + 1;
+    let x = x * 2;
     println!("The value of x is: {}", x);
 }
+
+// constants are values that are bound to a name and are not allowed to change, but there are a few differences between 
+// constants and variables. You aren't allowed to use mut w/ constants.
+
+// A scalar type represents a single value. Rust has four primary scalar types: 
+// integers, floating-point numbers, Booleans, and characters. You may recognize these from other programming languages.
