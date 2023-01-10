d0ddb66af49805c8c56d3695f69840e0f3edcb16
Tony LaForgia <alforgia428@gmail.com>
2023-01-09 18:52:24 -0600

    floating point type comments

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,11 @@ fn main() {
 
 // A scalar type represents a single value. Rust has four primary scalar types: 
 // integers, floating-point numbers, Booleans, and characters. You may recognize these from other programming languages.
+
+// integer types are numbers w/o a fractional component. u32 is an unsigned interger type. A signed integer type would start
+// with i instead of u.
+
+//floating-point types are f32 and f64, which are 32 bits and 64 bits in size, respectively. The default type is f64
+// because on modern CPUs it’s roughly the same speed as f32 but is capable of more precision.
+// The f32 type is a single-precision float, and f64 has double precision.
+
