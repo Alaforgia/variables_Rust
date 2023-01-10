a285167b8b48ae9ab552acecb8dd8bba9aeb9be6
Tony LaForgia <alforgia428@gmail.com>
2023-01-09 19:03:25 -0600

    tuple indexing

--- src/main.rs
+++ src/main.rs
@@ -53,3 +53,12 @@ fn main() {
 //  let (x, y, z) = tup;
 //  println!("The value of y is: {}", y);
 // }
+//
+// We can access a tuple element directly by using a period (.) followed by the index of the value we want to access. 
+// For example:
+// fn main() {
+//  let x: (i32, f64, u8) = (500, 6.4, 1);
+//  let five_hundred = x.0;
+//  let six_point_four = x.1;
+//  let one = x.2;
+//
