27d0f978420f1542cdb804e954de73587dc3126d
Tony LaForgia <alforgia428@gmail.com>
2023-01-09 19:00:47 -0600

    tuple type

--- src/main.rs
+++ src/main.rs
@@ -37,4 +37,19 @@ fn main() {
 //     let heart_eyed_cat = '�';
 //    }
 
+//A tuple is a general way of grouping together some number of other values with a variety of types into one compound type. 
+// Tuples have a fixed length: once declared, they cannot grow or shrink in size.
+// We create a tuple by writing a comma-separated list of values inside
+// parentheses. Each position in the tuple has a type, and the types of the different values in the tuple don’t have to be the same. 
+// We’ve added optional type annotations in this example:
+// fn main() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+//    }
 //
+// The variable tup binds to the entire tuple, because a tuple is considered a single compound element. 
+// To get the individual values out of a tuple, we can use pattern matching to destructure a tuple value, like this:
+// fn main() {
+//  let tup = (500, 6.4, 1);
+//  let (x, y, z) = tup;
+//  println!("The value of y is: {}", y);
+// }
